515bb2730138b6bfa19f75207427061e3a7b791f
imenmansouri93 <imennmansourii@gmail.com>
2024-07-03 17:48:01 +0100

    first commit

--- cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
@@ -1,21 +1,2 @@
 #!/bin/bash
-
-# Get the distribution ID
 id=$(lsb_release -i | cut -f 2-)
-echo "$id"
-
-# Check the number of lines in the script
-desired_lines=2
-actual_lines=$(wc -l < "$0")
-if [ "$actual_lines" -eq "$desired_lines" ]; then
-    echo "Desired number of lines: $desired_lines"
-    echo "Actual number of lines: $actual_lines"
-else
-    echo "Desired number of lines: $desired_lines"
-    echo "Actual number of lines: $actual_lines"
-fi
-
-# Print the last 5 characters of the file
-file_content=$(cat "$0")
-last_five_chars="${file_content: -5}"
-echo "Last 5 characters of the file: \"$last_five_chars\""
