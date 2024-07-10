7c357f5c2ca85133d8ab909bec26bbdef8d621bb
imenmansouri93 <imennmansourii@gmail.com>
2024-07-10 10:35:06 +0100

    new

--- network_security/0x01_passive_reconnaissance/4-dig_all.sh
+++ network_security/0x01_passive_reconnaissance/4-dig_all.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-dig +noall +answer @"8.8.8.8" "$1" ANY
+# Utilise le serveur DNS de Google (8.8.8.8) pour récupérer tous les enregistrements pour le domaine spécifié
+dig +noall +answer  $1 ANY
