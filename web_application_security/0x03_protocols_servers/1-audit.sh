ed80402315bc4208f7104cb5e8957f8688ec53e3
imenmansouri93 <imennmansourii@gmail.com>
2024-07-23 12:22:33 +0100

    new

--- web_application_security/0x03_protocols_servers/1-audit.sh
+++ web_application_security/0x03_protocols_servers/1-audit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E '^(PermitRootLogin|PasswordAuthentication|ChallengeResponseAuthentication|UsePAM|AllowTcpForwarding|X11Forwarding)' /etc/ssh/sshd_config | grep -vE 'PermitRootLogin no|PasswordAuthentication no|ChallengeResponseAuthentication no|UsePAM yes|AllowTcpForwarding no|X11Forwarding no'
+grep -E '^(PermitRootLogin|PasswordAuthentication|ChallengeResponseAuthentication|UsePAM|AllowTcpForwarding|X11Forwarding)' /etc/ssh/sshd_config | grep -Ev '^(#|$)' | grep -vE 'PermitRootLogin no|PasswordAuthentication no|ChallengeResponseAuthentication no|UsePAM yes|AllowTcpForwarding no|X11Forwarding no'
