99117b944e35ef6c8626e081f7645514b20857fc
imenmansouri93 <imennmansourii@gmail.com>
2024-07-23 12:55:20 +0100

    new

--- web_application_security/0x03_protocols_servers/5-snmp.sh
+++ web_application_security/0x03_protocols_servers/5-snmp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -type f -name '*snmp*.conf' -exec grep -i 'public' {} \; 2>/dev/null
+find / -type f \( -name '*snmp*.conf' -o -name '*snmp*.d' \) -exec grep -i 'public' {} \; 2>/dev/null
