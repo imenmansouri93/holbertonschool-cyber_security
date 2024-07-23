b72f09af4d55380ca62053b235109a27c635948e
imenmansouri93 <imennmansourii@gmail.com>
2024-07-23 13:01:00 +0100

    new

--- web_application_security/0x03_protocols_servers/6-smtp.sh
+++ web_application_security/0x03_protocols_servers/6-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -e "EHLO example.com\r\n" | openssl s_client -connect smtp.example.com:25 -starttls smtp 2>/dev/null | grep -q "250 STARTTLS" && echo "STARTTLS is configured." || echo "STARTTLS not configured."
+(echo -e "EHLO localhost\nQUIT" | openssl s_client -connect smtp.example.com:25 -starttls smtp 2>/dev/null) | grep -q "250-STARTTLS" && echo "STARTTLS is configured." || echo "STARTTLS not configured."
