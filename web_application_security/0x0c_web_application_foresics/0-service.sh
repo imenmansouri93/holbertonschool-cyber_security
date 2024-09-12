e75c8469e88686373a4cc449976b81886f7ac997
imenmansouri93 <imennmansourii@gmail.com>
2024-09-12 14:08:05 +0100

    new

--- web_application_security/0x0c_web_application_foresics/0-service.sh
+++ web_application_security/0x0c_web_application_foresics/0-service.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-grep -i -E 'sshd|ftp|httpd|nginx' auth.log | awk '{print $6}' | sort | uniq
+LOG_FILE="auth.log"
+SERVICES="sshd|ftp|httpd|nginx"
+
+# Output header
+echo "Scanning $auth.log for services: $SERVICES"
+echo
+
+# Scan the log file for relevant services and extract unique messages
+grep -i -E "$SERVICES" $"auth.log" | awk '{print $6}' | sort | uniq -c | sort -nr
+
+# Display the count of unique messages for each service
+echo
+echo "Summary of services found:"
+for SERVICE in sshd ftp httpd nginx; do
+    echo "$SERVICE:"
+    grep -i "$SERVICE" "$LOG_FILE" | awk '{print $6}' | sort | uniq -c | sort -nr
+    echo
+done
