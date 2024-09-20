854dccf5997c04781b31b729a2db3cf042385c04
imenmansouri93 <imennmansourii@gmail.com>
2024-09-20 13:48:42 +0100

    new

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep 'useradd' auth.log | awk -F'name=' '{print $2}' | awk '{print $1}' | awk '!seen[$0]++' | tr '\n' ',' | sed 's/,$//'
+grep 'useradd' auth.log | awk -F'name=' '{print $2}' | awk '{print $1}' | awk '!seen[$0]++' | sort |paste -sd ',' | sed 's/,,/,/g' | sed 's/,$//'
