bd567abb4f7270b559775086b488ff953b6136ef
imenmansouri93 <imennmansourii@gmail.com>
2024-09-24 12:24:39 +0100

    new

---
+++ scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
@@ -0,0 +1,12 @@
+class HelloWorld
+    def initialize
+        @message = "Hello, World!"
+    end
+
+    def print_hello
+        puts @message
+    end
+end
+
+hello = HelloWorld.new
+hello.print_hello
