feb1d72987ee03959d287cc908357b1c225dcafa
imenmansouri93 <imennmansourii@gmail.com>
2024-09-24 13:59:41 +0100

    new

---
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -0,0 +1,19 @@
+require 'json' 
+def count_user_ids(path)
+
+  file_content = File.read(path)
+  
+  
+  data = JSON.parse(file_content)
+  
+
+  user_id_count = Hash.new(0)
+
+  data.each do |entry|
+    user_id = entry["userId"]
+    user_id_count[user_id] += 1
+  end
+
+
+  user_id_count
+end
