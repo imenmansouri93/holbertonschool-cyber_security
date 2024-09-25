8deafa85391d4fc8c9b5d9a1ace4832e21dee827
imenmansouri93 <imennmansourii@gmail.com>
2024-09-25 09:15:55 +0100

    new

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -2,11 +2,11 @@ require 'json'
 
 
 def merge_json_files(file1_path, file2_path)
-  # Lire le contenu des deux fichiers
+
   file1_content = File.read(file1_path).strip
   file2_content = File.read(file2_path).strip
 
-
+  
   file1_data = file1_content.empty? ? [] : JSON.parse(file1_content)
   file2_data = file2_content.empty? ? [] : JSON.parse(file2_content)
 
