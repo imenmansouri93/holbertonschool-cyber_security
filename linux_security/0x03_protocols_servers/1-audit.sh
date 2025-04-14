f36edc890e0e860d3d3df77431f7bb76532d27da
imenmansouri93 <imennmansourii@gmail.com>
2025-04-14 08:58:11 +0100

    new

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sshd -T > /tmp/defaults
-grep -Ei '^[a-z]' /etc/ssh/sshd_config | while read l; do [[ "$(grep -i "^${l%% *} " /tmp/defaults)" != "$l" ]] && echo "$l"; done
+sshd -T > /tmp/defaults | grep -Ei '^[a-z]' /etc/ssh/sshd_config | while read l; do [[ "$(grep -i "^${l%% *} " /tmp/defaults)" != "$l" ]] && echo "$l"; done
